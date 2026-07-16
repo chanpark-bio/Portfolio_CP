65e739f720fa35960488b9ae3507849ba131a0e2
chanpark-bio <qkrcks801@gmail.com>
2026-07-16 23:20:50 +0900

    init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 echo "================================================="
-echo " 🧬 CP In Silico Hub - New Project Wizard"
+echo "  CP In Silico Hub - New Project Wizard"
 echo "================================================="
 
-# 인자(Arguments)를 직접 입력했는지 확인
 if [ "$#" -eq 3 ]; then
     PROJECT_NAME=$1
     TARGET_NAME=$2
     PDB_ID=$3
 else
-    # 인자가 없으면 대화형(Interactive)으로 물어보기
     echo "새로운 프로젝트 생성을 위한 정보를 입력해 주십시오."
     echo ""
     read -p "1. 프로젝트명을 입력하세요 (예: Cancer_DeNovo) : " PROJECT_NAME
@@ -19,9 +17,7 @@ else
     echo ""
 fi
 
-# 파이썬 스크립트 백그라운드 호출
 echo "⚙️  [$PROJECT_NAME\_$TARGET_NAME] 프로젝트 인프라를 구축합니다..."
-# 💡 [핵심 수정] 변경된 폴더 구조와 스크립트명(init_workspace.py) 적용
 python3 01_Library/Scripts/DeNovo_Pipeline/00_init_workspace.py \
     --project "$PROJECT_NAME" \
     --target "$TARGET_NAME" \
